f345558d06ae83413b8b5ee59b1075ad69ba0ec7
Pablo F.Pita <pablo.fernandezp00@csioviedo.com>
2023-06-04 11:34:29 +0200

    Sync timezones and storing

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 import csv
-import datetime
 import os
 import shutil
-import sys
 import time
-
 import numpy as np
 import pandas as pd
 from suntime import Sun, SunTimeException
@@ -29,11 +26,12 @@ def check_if_already_parsed(filename, header):
 
 
 def ask_to_continue(filename_original):
-    user_input = input("The file has already been parsed. Do you wish to continue and rewrite the data? [y/n] ")
+    user_input = input(f"The file {filename_original} has already been parsed. "
+                       f"Do you wish to continue and rewrite the data? [y/n] ")
     while user_input not in ["y", "n"]:
         print("Invalid input. Please enter 'y' or 'n' ")
-        user_input = input(f"The file {filename_original} has already been parsed. Do you wish to continue and rewrite "
-                           f"the data? [y/n] ")
+        user_input = input(f"The file {filename_original} has already been parsed. "
+                           f"Do you wish to continue and rewrite the data? [y/n] ")
     if user_input == "y":
         return True
     else:
@@ -103,6 +101,7 @@ def check_1440_records(file_path, output_ok_directory, output_error_directory):
 
 
 def get_sunrise_sunset(date):
+    # Coordinates of Gijón
     latitude = 43.5359
     longitude = -5.6619
     sun = Sun(latitude, longitude)
@@ -122,9 +121,9 @@ def get_sunrise_sunset(date):
 
 def revision(path_raw_data, new_file_name):
     df = pd.read_csv(path_raw_data, on_bad_lines='skip').dropna()
+
     # Format the Datetime column
     df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'], dayfirst=True)
-
     df.set_index('Datetime', inplace=True)
 
     first_row = df.head(1)  # First row does not follow the pattern
@@ -147,7 +146,7 @@ def revision(path_raw_data, new_file_name):
     # Reset index to Datetime
     new_df.reset_index(inplace=True)
 
-    # Missing the first few measures or the last few
+    # Missing the first or last few measures
     if len(new_df) != 1440:
         # Get the start and end time of the day
         start_time = new_df['Datetime'].min().replace(hour=0, minute=0, second=0, microsecond=0)
@@ -159,12 +158,8 @@ def revision(path_raw_data, new_file_name):
 
         # Merge the new DataFrame with the original DataFrame
         new_df = pd.merge(empty_df, new_df, left_index=True, right_on='Datetime', how='outer')
-        new_df.fillna(0.0, inplace=True)
-        # Round to 3 decimal places
-        new_df = new_df.round(decimals=3)
         # Reset index to Datetime
         new_df.set_index('Datetime', inplace=True)
-        # Reset index to Datetime
         new_df.reset_index(inplace=True)
 
     # Save the output DataFrame to a CSV file
@@ -172,8 +167,8 @@ def revision(path_raw_data, new_file_name):
     new_df.to_csv(path_treat_data, index=False)
 
 
-def datetime_together(path_raw_data, new_file_name):
-    df = pd.read_csv(path_raw_data)
+def datetime_together(path_input_data, new_file_name):
+    df = pd.read_csv(path_input_data)
     df['Datetime'] = pd.to_datetime(df['Date'] + ' ' + df['Time'], dayfirst=True)
 
     # Format the Datetime column
@@ -182,7 +177,7 @@ def datetime_together(path_raw_data, new_file_name):
     df.set_index('Datetime', inplace=True)
     # Drop 'Date' and 'Time' columns
     df = df.drop(['Date', 'Time'], axis=1)
-    # Reset index to Datetime
+
     df.reset_index(inplace=True)
     df.replace('---', 0.000, inplace=True)  # Replace non taken measures to 0.
     path_treat_data = os.path.join(PATH_TREAT_DATA, new_file_name)
@@ -190,7 +185,7 @@ def datetime_together(path_raw_data, new_file_name):
 
 
 def treating_data(filename):
-    file_name = os.path.basename(filename)
+    basefilename = os.path.basename(filename)
     df = pd.read_csv(filename)
     # print(df.dtypes)
     # Irradiance measurements columns
@@ -214,35 +209,105 @@ def treating_data(filename):
         df.loc[df['Datetime'] < sunrise_t, col] = 0
         df.loc[df['Datetime'] > sunset_t, col] = 0
 
-    file_path = os.path.join(PATH_DATA_GIJON_1440, file_name)
+    filename1440 = basefilename.split('Data')[0] + 'Data1440' + basefilename.split('Data')[1]
+    output = os.path.join(PATH_DATA_GIJON_1440, filename1440)
+    df.to_csv(output, index=False)
+    return output
 
-    df.to_csv(file_path, index=False)
+
+def resample_24UTC(filename):
+    df = pd.read_csv(filename)
+    # Convert 'Datetime' column to datetime type
+    df['Datetime'] = pd.to_datetime(df['Datetime'])
+
+    # Set 'Datetime' column as the index
+    df.set_index('Datetime', inplace=True)
+
+    # Resample to 1 record per hour and calculate the mean of each hour
+    df_resampled = df.resample('H').mean()
+    df_resampled = df_resampled.round(decimals=3)
+
+    # Reset the index to have 'Datetime' as a column again
+    df_resampled.reset_index(inplace=True)
+    basefilename = os.path.basename(filename)
+    filename24 = basefilename.split('Data')[0] + 'Data24UTC' + basefilename.split('Data')[1]
+    output = os.path.join(PATH_DATA_GIJON_24UTC, filename24)
+    # Save the resampled data to a new CSV file
+    df_resampled.to_csv(output, index=False)
 
 
-def push_into_db(filename, datalogger_db):
-    new_data = pd.read_csv(filename)
-    database = pd.read_csv(datalogger_db)
+def resample_24HLocal(path_file_1440):
+    df = pd.read_csv(path_file_1440)
+    # Convert 'Datetime' column to datetime type
+    df['Datetime'] = pd.to_datetime(df['Datetime'])
+    # Set 'Datetime' column as the index
+    df.set_index('Datetime', inplace=True)
+
+    df = df.tz_localize('utc')
+    df = df.tz_convert('Europe/Madrid')
+    # Resample to 1 record per hour and calculate the mean of each hour
+    df_resampled = df.resample('H').mean()
+    df_resampled = df_resampled.round(decimals=3)
+
+    # Reset the index to have 'Datetime' as a column again
+    df_resampled.reset_index(inplace=True)
+    # Remove the timezone from the column
+    df_resampled['Datetime'] = df_resampled['Datetime'].dt.tz_localize(None)
+
+    basefilename = os.path.basename(path_file_1440)
+    filename24 = basefilename.split('Data')[0] + 'Data24Local' + basefilename.split('Data')[1]
+    output = os.path.join(PATH_DATA_GIJON_24HORALOCAL, filename24)
+    # Save the resampled data to a new CSV file
+    df_resampled.to_csv(output, index=False)
+    return output
+
+
+def push_into_db1440(file1440utc, datalogger_db1440):
+    new_data = pd.read_csv(file1440utc)
+
+    database = pd.read_csv(datalogger_db1440)
     # Check if the new file has more columns than the database
     new_columns = set(new_data.columns) - set(database.columns)
     if new_columns:
         # Add the new columns to the database and set values to 0 for previous rows
         for column in new_columns:
-            database[column] = 0
+            database[column] = np.nan
 
-    # Remove duplicate rows from the new data based on 'Datetime' column
-    new_data.drop_duplicates(subset='Datetime', inplace=True)
-
-    # Append the new data to the database
-    database = pd.concat([database, new_data], ignore_index=True)
+    # Append the new data to the database, overwriting rows with the same 'Datetime'
+    database = pd.concat([database, new_data]).drop_duplicates(subset='Datetime', keep='last')
 
     # Order the database by 'Datetime' in ascending order
     database.sort_values(by='Datetime', inplace=True)
 
     # Delete columns with no values
     database.dropna(axis=1, how='all', inplace=True)
-    print(f"File: {os.path.basename(filename)}  has been added to database")
+    print(f"File: {os.path.basename(file1440utc)}  has been added to database\n")
     # Save the updated database back to the CSV file
-    database.to_csv(datalogger_db, index=False)
+    database.to_csv(datalogger_db1440, index=False)
+
+
+def push_into_db24(file24local, datalogger_db24):
+    new_data = pd.read_csv(file24local)
+
+    database = pd.read_csv(datalogger_db24)
+    # Check if the new file has more columns than the database
+    new_columns = set(new_data.columns) - set(database.columns)
+    if new_columns:
+        # Add the new columns to the database and set values to 0 for previous rows
+        for column in new_columns:
+            database[column] = np.nan
+
+    # Append the new data to the database, overwriting rows with the same 'Datetime'
+    database = pd.concat([database, new_data]).drop_duplicates(subset='Datetime', keep='last')
+
+    # Order the database by 'Datetime' in ascending order
+    database.sort_values(by='Datetime', inplace=True)
+
+    # Delete columns with no values
+    database.dropna(axis=1, how='all', inplace=True)
+    print(f"File: {os.path.basename(file24local)}  has been added to database\n")
+    # Save the updated database back to the CSV file
+    database.to_csv(datalogger_db24, index=False)
 
 
 def empty_csv_file(filename):  # DELETE LIST OF FILES (ONLY IN DEV MODE)
@@ -280,25 +345,33 @@ def main(file_full_path):
         delete_file(file_full_path)
         return error_message
 
-    # Decide whether the file is complete or not
+    # Decide whether the file is complete or not. And we copy it to a directory
     has1440 = check_1440_records(rawdata_file_path, PATH_TREAT_DATA, PATH_REVISE_DATA)
+    filename_raw_data = os.path.basename(rawdata_file_path)
     # In both cases we write a file on TratarDatos with the Date and Time columns fused into 1
     if not has1440:
-        filename_raw_data = os.path.basename(rawdata_file_path)
         file_path_revise = os.path.join(PATH_REVISE_DATA, filename_raw_data)
         revision(file_path_revise, filename_new)
     else:
         datetime_together(rawdata_file_path, filename_new)
+        # Delete unnecesary intermediary file
+        delete_file(os.path.join(PATH_TREAT_DATA, filename_raw_data))
 
     file_path_treat = os.path.join(PATH_TREAT_DATA, filename_new)
-    treating_data(file_path_treat)
-    file_path_treated = os.path.join(PATH_DATA_GIJON_1440, filename_new)
+    filename1440 = treating_data(file_path_treat)
+    #file_path_treated = os.path.join(PATH_DATA_GIJON_1440, filename_new)
+    # Convert into 24 rows per day. UTC time and Localtime
+    resample_24UTC(filename1440)
+    localtime_filename = resample_24HLocal(filename1440)
     # Check which database the file is going to be popped into
     if "Estaci" in filename_new:
-        push_into_db(file_path_treated, DATALOGGER1_DB)
+        push_into_db1440(filename1440, DATALOGGER1_DB)
+        push_into_db24(localtime_filename, DATALOGGER1_DB24)
     else:
-        push_into_db(file_path_treated, DATALOGGER2_DB)
+        push_into_db1440(filename1440, DATALOGGER2_DB)
+        push_into_db24(localtime_filename, DATALOGGER2_DB24)
     delete_file(file_full_path)
+    print()
 
 
 if __name__ == '__main__':
@@ -309,15 +382,16 @@ if __name__ == '__main__':
     PATH_REVISE_DATA = r"C:\Users\pablo\Documents\Teleco2018-\5CURSO\TFG\RevisarDatos"
     PATH_TREATED_DATA = r"C:\Users\pablo\Documents\Teleco2018-\5CURSO\TFG\TratarDatos\DatosTratados"
     PATH_DATA_GIJON_1440 = r"C:\Users\pablo\Documents\Teleco2018-\5CURSO\TFG\TratarDatos\DatosTratados\DatosTratados_1440_Gijon"
-    PATH_DATA_GIJON_24 = r"C:\Users\pablo\Documents\Teleco2018-\5CURSO\TFG\TratarDatos\DatosTratados\DatosTratados_24_Gijon"  r""
+    PATH_DATA_GIJON_24UTC = r"C:\Users\pablo\Documents\Teleco2018-\5CURSO\TFG\TratarDatos\DatosTratados\DatosTratadosUTC_24_Gijon"
+    PATH_DATA_GIJON_24HORALOCAL = r"C:\Users\pablo\Documents\Teleco2018-\5CURSO\TFG\TratarDatos\DatosTratados\DatosTratadosHoraLocal_24_Gijon"
     DATALOGGER1_DB = r"C:\Users\pablo\Documents\Teleco2018-\5CURSO\TFG\TratarDatos\DatosTratados\Datalogger1.csv"
     DATALOGGER2_DB = r"C:\Users\pablo\Documents\Teleco2018-\5CURSO\TFG\TratarDatos\DatosTratados\Datalogger2.csv"
+    DATALOGGER1_DB24 = r"C:\Users\pablo\Documents\Teleco2018-\5CURSO\TFG\TratarDatos\DatosTratados\Datalogger24hLocalEstaci.csv"
+    DATALOGGER2_DB24 = r"C:\Users\pablo\Documents\Teleco2018-\5CURSO\TFG\TratarDatos\DatosTratados\Datalogger24hLocal.csv"
 
     while True:
         for file in os.listdir(PATH_INPUT_FILES):
             if file.lower().endswith('.csv'):
                 file_to_process = os.path.join(PATH_INPUT_FILES, file)
                 main(file_to_process)
-        empty_csv_file('ListOfParsedCSV_dat2.CSV')  # To not have to clean it myself. DEV
-        empty_csv_file('ListOfParsedCSV_dat1.CSV')
         time.sleep(10)
